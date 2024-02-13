4de1edb39e5f79b4ed090285d761876a2ba048cf
mannadevs <msdmanna77@gmail.com>
2024-02-13 11:19:28 +0600

    not working dropdown

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -54,3 +54,80 @@ if (trigger || dropdown) {
         trigger.removeClass("active");
     });
 }
+
+$(document).ready(function () {
+    $("#txtInput, #txtInput2").keypress(function (event) {
+        if (event.which === 13) {
+            // Enter key pressed
+            var keyword = $(this).val().trim();
+            if (keyword !== "") {
+                $("#keywordsContainer, #keywordsContainer2").append(
+                    '<div class="keyword">' +
+                        keyword +
+                        '<span class="remove-icon">&#10006;</span></div>'
+                );
+                $(this).val("");
+            }
+        }
+    });
+
+    $("#keywordsContainer, #keywordsContainer2").on(
+        "click",
+        ".remove-icon",
+        function () {
+            $(this).parent(".keyword").remove();
+        }
+    );
+});
+
+$(document).ready(function () {
+    $(".dropdown-item").each(function () {
+        var $dropdown = $(this).find(".dropdown");
+        var $dropdownList = $(this).find(".dropdown-list");
+        var $dropdownMain = $(this).find(".dropdown-main");
+
+        // Click event handler for dropdown list items
+        $dropdownList.find("li label").click(function (event) {
+            var selectedText = $(this).text().trim();
+            $dropdownMain.text(selectedText);
+            $dropdownList.hide();
+            event.stopPropagation(); // Prevent the click event from propagating to the document
+        });
+
+        // Click event handler for the document to hide dropdown list when clicked outside
+        $(document).click(function (event) {
+            // Check if the clicked element is not within the dropdown list or dropdown
+            if (
+                !$(event.target).closest(".dropdown-list").length &&
+                !$(event.target).closest(".dropdown").length
+            ) {
+                $dropdownList.hide();
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $(".more-filter-btn, .close-btn-two").click(function () {
+        $(".more-filter").toggleClass("active");
+    });
+});
+
+$(document).ready(function () {
+    $(".thumb").click(function () {
+        // Remove 'active' class from all thumbnails within the same structure
+        $(this).siblings().removeClass("active");
+
+        // Add 'active' class to the clicked thumbnail
+        $(this).addClass("active");
+
+        // Get the src attribute of the clicked thumbnail
+        var newSrc = $(this).find("img").attr("src");
+
+        // Set the src attribute of the main profile image within the same structure
+        $(this)
+            .closest(".single-user-profile-item__thumb")
+            .find(".main-thumb")
+            .attr("src", newSrc);
+    });
+});
